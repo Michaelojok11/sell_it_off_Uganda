bdf5694b157e1d1bf918efc17bcfee463f12824d
Michaelojok11 <michaelocan11@gmail.com>
2023-08-21 22:33:39 +0300

    update git

--- sell-it-off-Uganda/script.js
+++ sell-it-off-Uganda/script.js
@@ -1,1 +1,13 @@
-// Script for navigation bar
+// hamburger menu making it clickable
+const bar = document.getElementById('bar');
+// on clicking the hamburger I want to see the Navbar
+const nav = document.getElementById('navbar');
+
+// check if the navbar is showing
+ if (bar){
+    // if the bar is clicked (event listener), then give me the function
+    bar.addEventListener('click', ()=> {
+        nav.classList.add('active');
+
+    } )
+ }
