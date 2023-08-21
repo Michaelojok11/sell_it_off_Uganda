0c7240b8ad0a3f360d33d02a65eea934f17ca729
Michaelojok11 <michaelocan11@gmail.com>
2023-08-21 22:38:07 +0300

    update git

--- sell-it-off-Uganda/script.js
+++ sell-it-off-Uganda/script.js
@@ -1,5 +1,6 @@
 // hamburger menu making it clickable
 const bar = document.getElementById('bar');
+const close = document.getElementById('close');
 // on clicking the hamburger I want to see the Navbar
 const nav = document.getElementById('navbar');
 
@@ -8,6 +9,12 @@ const nav = document.getElementById('navbar');
     // if the bar is clicked (event listener), then give me the function
     bar.addEventListener('click', ()=> {
         nav.classList.add('active');
+    } )
+ }
 
+ if (close){
+    // if the bar is clicked (event listener), then give me the function
+    close.addEventListener('click', ()=> {
+        nav.classList.remove('active');
     } )
  }
